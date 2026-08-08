5f0e51cda5b6438b4bd76bdb111787f6353913e7
NinaH41 <ninah41@nycstudents.net>
2026-08-08 10:56:58 -0400

    Third commit, fixed commands and some imports

--- src/main/java/frc/robot/Hooper.java
+++ src/main/java/frc/robot/Hooper.java
@@ -1,65 +1,66 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
 import edu.wpi.first.wpilibj.motorcontrol.PWMTalonFX;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.HopperConstants;
+import frc.robot.HopperConstants;
+import frc.robot.Port.HopperPort;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
+
 
 public class Hooper extends SubsystemBase {
+private TalonFX indexerMotor; 
+private TalonFX hopporMotor;
+private RobotContainer RobotContainer;
+private HopperConstants HopperConstants;
+private Port Port;
+private PWMMotorController indexMotor;
 
-private final Talon indexerMotor; 
-private final  Talon hopporMotor;
 
 public Hooper( ) {
-hopperMotor = new TalonFX hopporMotor;
+hopporMotor = new TalonFX hopporMotor;
 indexerMotor = new TalonFX indexerMotor;
-hopperMotor = new TalconFX(HopperConstants.MOTOR_ID);
+hopporMotor = new TalconFX(HopperConstants.MOTOR_ID);
 indexerMotor = new TalconFX(HopperConstants.MOTOR_ID);
-hopperMotor.getConfigurator().apply(TalonFXConfiguration);
+hopporMotor.getConfigurator().apply(TalonFXConfiguration);
 indexerMotor.getConfigurator().apply(TalonFXConfiguration);
 Port Port  = new Port();
 RobotContainer  = new RobotContainer();
+Constants = new Constants();
+
 
 }
 
 
 public Command runIndexer() {
-return this.run(()-> ) {
-indexerMotor.set(HopperConstants.INDEXERMOTOR_SPEED);
+return this.run(() -> indexerMotor.set(HopperConstants.MOTORSPEED));
 }
-}
+
 public Command reverseIndexer() {
-return this.run(()-> ) {
-indexerMotor.set(!HopperConstants.MOTOR_SPEED );
+return this.run(()-> indexerMotor.set(!HopperConstants. ));
 }
 
 
 public Command stopIndexer() {
-return this.run(()->){
-indexerMotor.set ();
+return this.runOnce(() -> indexMotor.set(0));
 	}
-}
+
 
 public Command runHopperMotor() {
-return this.run(() -> ) {
-hopperMoter.set(HopperConstants.MOTOR_SPEED);
+return this.run(() -> hopporMotor.set(HopperConstants.MOTOR_SPEED));
 	}
-}
 
 public Command stopHopperMotor() {
-return this.run(()->) {
-hopperMotor.set();
+return this.runOnce(()-> hopporMotor.set(0));
 	}
-}
+
 
 
 public Command reverseHopperMotor() {
-return this.run((!HopperConstants.HOPPERMOTOR_SPEED) -> ) {
-hopperMoter.set();
+return this.run(() -> hopporMotor.set(!HopperConstants.HOPPERMOTOR_SPEED));
 	}
 }
-
- }
-}
+ 
 
---
+++ src/main/java/frc/robot/HopperConstants.java
@@ -0,0 +1,22 @@
+
+package frc.robot;
+import edu.wpi.first.wpilibj.CAN;
+
+public class HopperConstants {
+
+	public static class HpperConstants {
+
+	public static final CAN canbus = new CANBus("rio");
+      private static final double MOTOR_SPEED = 0.5;
+       private static final int HopperMOTOR_SPEED_Limitation  = 40;
+       private static final int IndexerMOTOR_SPEED_Limitation  = 30;
+       public static final double INDEXERMOTOR_SPEED =
+       
+
+}
+
+    public static final String MOTORSPEED = null;
+
+      public static Object MOTOR_SPEED;
+}
+
