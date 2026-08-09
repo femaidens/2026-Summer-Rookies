7ede63bb6e6b5bdb355f6c4257ef034d0a057b62
NinaH41 <ninah41@nycstudents.net>
2026-08-09 11:40:28 -0400

    9th commit

--- src/main/java/frc/robot/Hopper.java
+++ src/main/java/frc/robot/Hopper.java
@@ -1,17 +1,19 @@
 package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.HopperConstants;
 import frc.robot.Port.HopperPort;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 
 
-public class Hopper extends SubsystemBase private final TalonFX indexerMotor; 
-private final TalonFX hopperMotor;
-private RobotContainer RobotContainer;
-private Constants HopperConstants;
-private Port Port;
+public class Hopper extends SubsystemBase {
+  private final TalonFX indexerMotor;
+  private final TalonFX hopperMotor;
+  private RobotContainer RobotContainer;
+  private Constants HopperConstants;
+  private Port Port;
 
 
 
@@ -19,11 +21,11 @@ public Hopper( ) {
 TalonFXConfiguration config = new TalonFXConfiguration();
 indexerMotor = new TalonFX(HopperPort.INDEXER_MOTOR, HopperConstants.canbus);
 hopperMotor= new TalonFX(HopperPort.HOPPER_MOTOR, HopperConstants.canbus);
-hopperMotor.getConfigurator().apply(TalonFXConfiguration);
-indexerMotor.getConfigurator().apply(TalonFXConfiguration);
+indexerMotor.getConfigurator().apply(config);
+hopperMotor.getConfigurator().apply(config);
 Port Port  = new Port();
 RobotContainer  = new RobotContainer();
-HopperConstants = new HopperConstants();
+HopperConstants = new Constants();
 
 }
 
