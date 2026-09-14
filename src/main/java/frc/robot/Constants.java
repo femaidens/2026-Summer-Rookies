093b5a5523aab764e35de340c498e66396149fb2
Saloni-Kelkar-01 <kelkarsaloni@gmail.com>
2026-09-13 22:45:55 -0400

    8/17/2026 Third draft, here: set values in constants file instead of hardcoding, actually included imports in intake file, added a better version of PID instead of the previous mockup, updated REVLib and specified motors as MotorType.kBrushless, added methods for PID, structured so periodic handles safety then PID when enabled, ATTEMPTED an auton section, completed command syntax

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,8 +2,6 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
-
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -12,19 +10,17 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
+package frc.robot;
+
 public final class Constants{
-  //Pivot measurements 
-  public static final double MAX_PIVOT_HEIGHT = 8.0;  
-  public static final double MIN_PIVOT_HEIGHT = 0.2; 
-  //PID   
-  public static final double K_P = 0.001;   
-  public static final double K_I = 0.0; 
-  public static final double K_D = 0.001;
+//Pivot measurements 
+public static final double MAX_PIVOT_HEIGHT = 8.0;  
+public static final double MIN_PIVOT_HEIGHT = 0.2; 
+//PID   
+public static final double K_P = 0.001;   
+public static final double K_I = 0.0; 
+public static final double K_D = 0.001;
+}
 
-  public static final class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
-
-  }
 
   
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,127 +1,243 @@
+package frc.robot.subsystems;
+import frc.robot.Constants;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkAbsoluteEncoder;
+
+
 public class Intake extends SubsystemBase{
 
-	//FIELD 
-		private final SparkMax rollerMotor;
-		private final SparkMax pivotMotor;
-		private final AbsoluteEncoder absoluteEncoder;
-	
-	public Intake(){
-	//CONSTRUCTER
-	rollerMotor = new SparkMax(1);
-		pivotMotor = new SparkMax(2);
-		absoluteEncoder = pivotMotor.getAbsoluteEncoder(2);
+//FIELD 
+	private final SparkMax rollerMotor;
+	private final SparkMax pivotMotor;
+	private final SparkAbsoluteEncoder absoluteEncoder;
+	//private final IntakeIO hardware; 
+private final PIDController PID = new PIDController(Constants.K_P, Constants.K_I, Constants.K_D);
+private double setPoint;
+//if we're implementing PID:
+private boolean PIDEnabled = false;
+//if auton
+private boolean autonEnded = false;
+
+public Intake(){
+//CONSTRUCTER
+rollerMotor = new SparkMax(1, MotorType.kBrushless);
+	pivotMotor = new SparkMax(2, MotorType.kBrushless);
+absoluteEncoder = pivotMotor.getAbsoluteEncoder();
+
+}
+
+//METHODS
+//PIVOT MOTOR: 
+//PIVOT HELPER METHODS  
+public void helperGoUp(){ 
+setSpeed(0.5); 
+}
+
+public void helperGoDown(){
+setSpeed(-0.5); 
+}
+
+public double getAbsolutePosition(){
+	return absoluteEncoder.getPosition();
+}
+
+public void setPoint(double setPoint) {
+    this.setPoint = setPoint;
+}
+
+public boolean isPIDEnabled(){
+	return PIDEnabled; //cause if it is enabled this value will be true, if it isn't it'll be false
+}
+
+//PIVOT SAFETY METHODS:  
+//this is the reactive safety measure just in case something unexpected happens    
+public boolean safetyCheck(){
+if (getAbsolutePosition() > Constants.MAX_PIVOT_HEIGHT){
+	helperGoDown(); 
+return true;
 	}
-	
-	//METHODS
-	//PIVOT MOTOR: 
-	//PIVOT HELPER METHODS  
-	public void helperGoUp(){ 
-	setSpeed(0.5); 
+else if (getAbsolutePosition() < Constants.MIN_PIVOT_HEIGHT){
+	helperGoUp();
+	return true;
 	}
-	
-	public void helperGoDown(){
-	setSpeed(-0.5); 
+	else{
+	return false;
 	}
-	
-	public double getAbsolutePosition(){
-		return absoluteEncoder.getPosition();
-	}
-	
-	//PIVOT SAFETY METHODS: 
-	//this is the reactive safety measure just in case something unexpected happens    
-	public void safetyCheck(){
-		if (getAbsolutePosition() > MAX_PIVOT_HEIGHT){
-		helperGoDown(); 
-		}
-	else if (getAbsolutePosition() < MIN_PIVOT_HEIGHT){
-		helperGoUp();
-		}
-	}
-	//makes this run in the background so it automatically fixes anything at any time
-	@Override 
-	public void periodic(){
-		safetyCheck();
-	}
-	//checks if it can actually go up or down, this is the preventative safety measure so we don't command anything wrong  
-	public boolean canGoUp() {
-		if (getAbsolutePosition() >= MAX_PIVOT_HEIGHT) {
-			return false;
-		}
-		return true;
-	}
-	
-	public boolean canGoDown() {
-		if (getAbsolutePosition() <= MIN_PIVOT_HEIGHT) {
-			return false;
-		}
-		return true;
-	}
-	//emergency stop
-	public void stopPivot(){
-	pivotMotor.set(0.0);
-	}
-	
-	//PIVOT CONVENIENCE METHODS  
-	//if the driver wants to set it to a specific speed, here it is
-	public void setSpeed(double speed) {
-		pivotMotor.set(MathUtil.clamp(speed, -0.5, 0.5));
-	}
-	//temporary PID setup (it's pseudocode so i'll make it more accurate later)
-	public void goToExactPosition(double setAngle){
-	  if (getAbsolutePosition() > setAngle){
-	helperGoDown();
-	}
-	  else if (getAbsolutePosition() < setAngle){
+}
+//makes this run in the background so it automatically fixes anything at any time
+@Override 
+public void periodic(){
+if (safetyCheck() == true){
+return;
+}
+//if we want PID
+if (isPIDEnabled() == true){
+	runPID();
+}
+}
+
+//checks if it can actually go up or down, this is the preventative safety measure so we don't command anything wrong  
+public boolean canGoUp() {
+    if (getAbsolutePosition() >= Constants.MAX_PIVOT_HEIGHT) {
+        return false;
+    }
+    return true;
+}
+
+public boolean canGoDown() {
+    if (getAbsolutePosition() <= Constants.MIN_PIVOT_HEIGHT) {
+        return false;
+    }
+    return true;
+}
+//emergency stop
+public void stopPivot(){
+pivotMotor.set(0.0);
+}
+
+//PIVOT CONVENIENCE METHODS  
+//if the driver wants to set it to a specific speed, here it is
+public void setSpeed(double speed) {
+    pivotMotor.set(MathUtil.clamp(speed, -0.5, 0.5));
+}
+
+//OKAY SO I GET THAT A SIMPLE INTAKE DOESN'T NEED PID, BUT I WAS JUST TRYING TO SEE IF I COULD MAKE ONE. OTHERWISE IGNORE THIS:
+public void runPID(){
+double currentPosition = getAbsolutePosition();
+double PID_Output = PID.calculate(currentPosition, setPoint);  
+
+PID_Output = MathUtil.clamp(PID_Output,-0.5,0.5); 
+setSpeed(PID_Output);
+}
+//AGAIN I'LL PROB LEAVE THIS OUT WHEN I PUSH THE CODE INTO THE REPO, UNLESS U THINK IT NEEDS TO BE HERE
+
+//PIVOT COMMANDABLE METHODS:   
+public void goUp(){
+	if (canGoUp() == true){
 	helperGoUp();
 	}
-	  else if (getAbsolutePosition() == setAngle){
-		setSpeed(0);
-		}
+}
+public void goDown(){
+	if (canGoDown() == true){
+	helperGoDown();
 	}
-	
-	//PIVOT COMMANDABLE METHODS:
-	public void goUp(){
-		if (canGoUp() == true){
-		helperGoUp();
-		}
+}
+//these literally just make the pivot move up/down until it physically can't anymore via canGoUp/down
+
+public boolean turnPIDon(){
+	PIDEnabled = true;
+	return true; 
+}
+
+public boolean turnPIDoff(){
+	PIDEnabled = false;
+	return false;
+}
+
+public void goUpToPosition(double amount){
+	if (canGoUp() == true){
+	setPoint = getAbsolutePosition() + amount;
+PIDEnabled = true;
+}
+}
+public void goDownToPosition(double amount){
+	if (canGoDown() == true){
+	setPoint = getAbsolutePosition() - amount;
+PIDEnabled = true;
+}
+}
+
+//ROLLER MOTOR:    
+public void intakeFuel() {
+    rollerMotor.set(-0.5);
+}
+
+public void ejectFuel() {
+    rollerMotor.set(0.5);
+}
+
+public void stopRollers() {
+    rollerMotor.set(0);
+}
+
+//COMMANDS: 
+public Command goUpCmd(){
+	return this.run(() -> goUp());
 	}
-	public void goDown(){
-		if (canGoDown() == true){
-		helperGoDown();
-		}
+
+public Command goDownCmd(){
+	return run(() -> goDown());
 	}
-	//ROLLER MOTOR 
-	
-	public void intakeFuel() {
-		rollerMotor.set(-0.5);
+
+public Command stopPivotCmd(){
+	return runOnce(() -> stopPivot());
 	}
-	
-	public void ejectFuel() {
-		rollerMotor.set(0.5);
+
+public Command stopRollersCmd(){
+	return runOnce(() -> stopRollers());
 	}
-	
-	public void stopRollers() {
-		rollerMotor.set(0);
+
+public Command intakeFuelCmd(){
+	return run(() -> intakeFuel());
 	}
-	
-	//COMMANDS: 
-	public Command goUpCmd(){
-		return run(() -> intake.goUp());
-		}
-	
-	public Command goDownCmd(){
-		return run(() -> intake.goDown());
-		}
-	
-	public Command stopPivotCmd(){
-		return run(() -> intake.stopPivot());
-		}
-	
-	public Command intakeFuelCmd(){
-		return run(() -> intake.intakeFuel());
-		}
-	
-	public Command ejectFuelCmd(){
-		return run(() -> intake.ejectFuel());
-		}
-	
+
+public Command ejectFuelCmd(){
+	return run(() -> ejectFuel());
+	}
+
+public Command goUpToPositionCmd(double amount){
+	return runOnce(() -> goUpToPosition(amount));
+	}
+
+public Command goDownToPositionCmd(double amount){
+	return runOnce(() -> goDownToPosition(amount));
+	}
+//OKAY I ALSO GET WE PROB DON'T NEED STUFF FOR AUTON CAUSE IN THE ACTUAL GAME IM ASSUMING WE'LL FILL UP THE ROBOT WITH BALLS FROM OUR CHAMBER TO SHOOT INSTEAD OF INTAKING THEM, BUT JUST IN CASE I WANTED TO CHECK IF THIS MAKES SENSE: 
+
+//helper methods
+public void startAuton(){
+    autonEnded = false;
+}
+
+public void endAuton(){
+    autonEnded = true;
+}
+
+public boolean isAutonOn(){
+return !autonEnded; //cause again if this is true method returns true if false it returns false 
+}
+
+public boolean checkForFuel(){
+	if (//camera spots fuel//ignore this part idk how the camera works, and I already know that we aren't using a camera for our intake so double emphasis on this being theoretical//){
+		return true;
+	}
+	else{
+		return false;
+}
+}
+
+//commands   
+public Command endAutonCmd(){
+	return runOnce(()-> {
+	endAuton(); 
+	});
+}
+
+public Command autonFuelIntake(){	
+return run(()-> {
+if (isAutonOn() && checkForFuel() == true){
+intakeFuel();
+}
+else{
+stopRollers();
+}
+
+});
+
+}
